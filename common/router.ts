fc5582c9f69002955ac5d2132b1cd46ba7d4ad10
Wendel Macedo <wendelmacedooliveira@hotmail.com>
2020-04-28 23:41:18 -0300

    Adiçao do metodo DELETE e refatoraçao de algumas partes do router

--- common/router.ts
+++ common/router.ts
@@ -1,5 +1,18 @@
 import * as restify from "restify";
+import { EventEmitter } from "events";
 
-export abstract class Router {
+export abstract class Router extends EventEmitter {
   abstract applyRoutes(application: restify.Server);
+
+  render(request: restify.Request, response: restify.Response, next: restify.Next) {
+    return (document: any) => {
+      if (document) {
+        this.emit("beforeRender", document);
+        response.json(document);
+      } else {
+        response.json(404);
+      }
+      return next();
+    };
+  }
 }
--- users/users.router.ts
+++ users/users.router.ts
@@ -3,31 +3,43 @@ import * as restify from "restify";
 import { User } from "./users.model";
 
 class UsersRouter extends Router {
+  constructor() {
+    super();
+
+    this.on("beforeRender", (document) => {
+      document.password = undefined;
+    });
+  }
+
   applyRoutes(application: restify.Server) {
     application.get("/users", (req, resp, next) => {
-      User.find().then((users) => {
-        resp.json(users);
-        return next();
-      });
+      User.find().then(this.render(req, resp, next));
     });
 
     application.get("/users/:id", (req, resp, next) => {
-      User.findById(req.params.id).then((user) => {
-        if (user) {
-          resp.json(user);
-          return next();
-        }
-        resp.send(404, { message: "Usuário não encontrado" });
-      });
+      User.findById(req.params.id).then(
+        this.render(req, resp, next)
+
+        //   (user) => {
+        //   if (user) {
+        //     resp.json(user);
+        //     return next();
+        //   }
+        //   resp.send(404, { message: "Usuário não encontrado" });
+        // }
+      );
     });
 
     application.post("/users", (req, resp, next) => {
       let user = new User(req.body);
-      user.save().then((user) => {
-        user.password = undefined;
-        resp.json(user);
-        return next();
-      });
+      user.save().then(
+        this.render(req, resp, next)
+        //   (user) => {
+        //   user.password = undefined;
+        //   resp.json(user);
+        //   return next();
+        // }
+      );
     });
 
     application.put("/users/:id", (req, resp, next) => {
@@ -42,22 +54,32 @@ class UsersRouter extends Router {
           }
           resp.send(404);
         })
-        .then((user) => {
-          resp.json(user);
-          return next();
-        });
+        .then(this.render(req, resp, next));
     });
 
     application.patch("/users/:id", (req, resp, next) => {
       const options = { new: true };
-      User.findByIdAndUpdate(req.params.id, req.body, options).then((user) => {
-        if (user) {
-          resp.json(201, user);
+      User.findByIdAndUpdate(req.params.id, req.body, options).then(
+        this.render(req, resp, next)
+
+        //   (user) => {
+        //   if (user) {
+        //     resp.json(201, user);
+        //     return next();
+        //   }
+        //   resp.json(404, { message: "Usuário não encontrado" });
+        //   return next();
+        // }
+      );
+    });
+
+    application.del("/users/:id", (req, res, next) => {
+      User.remove({ _id: req.params.id })
+        .exec()
+        .then((cmdResult: any) => {
+          cmdResult.result.n ? res.send(204) : res.send(404);
           return next();
-        }
-        resp.json(404, { message: "Usuário não encontrado" });
-        return next();
-      });
+        });
     });
   }
 }
